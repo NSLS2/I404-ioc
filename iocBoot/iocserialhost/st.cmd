99278e83ea206069f4a55bb009fc62a14e9dc185
dchabot <dchabot@localhost>
2010-05-26 11:41:16 -0400

    add save/restore machinery

--- iocBoot/iocserialhost/st.cmd
+++ iocBoot/iocserialhost/st.cmd
@@ -27,5 +27,17 @@ asynOctetSetOutputEos("COM1",0,"\r\n")
 # Load record instances
 dbLoadTemplate("db/I404.substitutions")
 
+# autosave/restore mechanisms
+#save_restoreSet_Debug(0)
+#save_restoreSet_IncompleteSetsOk(1)
+#save_restoreSet_DatedBackupFiles(1)
+
+#set_savefile_path("${TOP}/as","/save")
+#set_requestfile_path("${TOP}/as","/req")
+
+#set_pass1_restoreFile("modules_I404.sav")
+
+#cd ${TOP}/iocBoot/${IOC}
 iocInit()
 
+#create_monitor_set("modules_I404.req", 15 , "")
